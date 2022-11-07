f388333d1228ce1ceebf199ca021243d8e070f14
Rocky-1617 <rakesh.adepu@slu.edu>
2022-11-07 12:44:32 -0600

    Added functionalities for add, update and delete

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -1,39 +1,49 @@
-class todoservice{
+class todoservice {
     todo_data = {
-        "todo":[{
+        "todo": [{
             "title": "T1",
             "description": "D1",
             "done": false
-        },{
+        }, {
             "title": "T1",
             "description": "D1",
             "done": false
-        },{
+        }, {
             "title": "T1",
             "description": "D1",
             "done": false
         }]
     }
-    constructor(){
-        this.todos=this.todo_data;
+    constructor() {
+        this.todos = this.todo_data;
     }
 
-    get_todos(){
+    get_todos() {
         return this.todos;
     }
 
-    add_todo(todo){
+    add_todo(todo) {
         // Your code here
+        // this.todo_data.push(todo);
+        // console.log(todo_data);
+        this.todo_data["todo"].push(todo)
+        this.todos = this.todo_data
+
     }
 
-    delete_todo(id){
+    delete_todo(id) {
         // Your code here
+        // this.todo_data = this.todo_data.filter(item => item.id != id)
+        // console.log(todo_data);
+        this.todos.splice(this.todos.indexOf(id))
+        this.todos = this.todo_data
     }
 
-    update_todo(id, todo){
+    update_todo(id, todo) {
         // Your code here
+        this.todos.map(p => p.id === id ? p.todo = todo : p)
     }
 }
 
 
-module.exports= todoservice;
+module.exports = todoservice;
